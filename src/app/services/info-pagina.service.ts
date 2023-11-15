350088f167f2434e2e51cdc8ed97963e0a2e1b03
Miguel Porras <mapa198907@gmail.com>
2023-11-15 14:08:38 -0600

    quinta seccion terminada

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,22 +9,37 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   loaded = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
 
-    // console.log('Servicio de infoPagina');
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
 
     // Leer el archivo JSON:
 
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina ) => {
-
       this.loaded = true;
       this.info = resp;
-      console.log( resp );
-
     });
 
+  }
+
+  private cargarEquipo() {
+
+    // Leer el archivo JSON:
+
+    this.http.get('https://angular-html-f6e78-default-rtdb.firebaseio.com/equipo.json')
+     .subscribe( (resp: any) => {
+
+      this.equipo = resp;
+      console.log( resp );
+    });
 
   }
 }
